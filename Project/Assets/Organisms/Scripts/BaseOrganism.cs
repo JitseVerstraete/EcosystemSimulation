6928aeae02d176561279c6a6c464b86ec39feaae
jitseV <jitse.verstraete@gmail.com>
2022-01-21 17:09:23 +0100

    initial project commit

---
+++ Project/Assets/Organisms/Scripts/BaseOrganism.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BaseOrganism : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //make a custom Update that Updates the organism for one time step
+    protected virtual void UpdateTimeStep()
+    {
+
+    }
+
+}
